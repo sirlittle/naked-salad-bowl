99809d17b7bb39f7b47d4f9a73d3cb09fc13b988
Salil Vanvari <salilvanvari@berkeley.edu>
2020-05-05 01:47:13 -0700

    Changed Splash Page to tabbed change between Join Room and Create Room. Added a JoinRoomCard Component to more easily control content that will be displayed for joining the room. That is still tbd implementation.

---
+++ src/JoinRoomCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./css/main.css";
+const firebase = require("firebase");
+require("firebase/firestore");
+
+class JoinRoomCard extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        Join Room Will Go Heres
+      </div>
+    );
+  }
+}
+
+export default JoinRoomCard;
--- src/SplashPage.js
+++ src/SplashPage.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import CreateRoomCard from './CreateRoomCard.js'
+import JoinRoomCard from './JoinRoomCard.js'
+
 import "./css/main.css";
 
 class SplashPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      joinRoomClicked: true,
+    };
+  }
   createGame() {
-    console.log("Creating Game");
-    window.location = window.location + "create";
+    this.setState({ joinRoomClicked: false })
+    console.log("Creating Game Card Shown");
   }
 
   joinGame() {
-    console.log("Joining Game");
+    this.setState({ joinRoomClicked: true })
+    console.log("Joining Game Card Shown");
   }
 
-  render() {
+  render() { 
+    var joinRoomClicked = this.state.joinRoomClicked;
     return (
-    <div className="wrapper">
-      <div className="parent inline-flex-parent">  
-        <button onClick={this.createGame} className="button">Create Game</button>
-        <button onClick={this.joinGame} className="button">Join Game</button>
+      <div className="wrapper">
+        <div className="parent inline-flex-parent">  
+          <button onClick={() => this.createGame()} className="button">Create Game</button>
+          <button onClick={() => this.joinGame()} className="button">Join Game</button>
+        </div>
+        {!joinRoomClicked && <CreateRoomCard />}
+        {joinRoomClicked && <JoinRoomCard />}
       </div>
-    </div>
     );
   }
 }
