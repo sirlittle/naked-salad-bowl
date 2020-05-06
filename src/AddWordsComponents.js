ff319c0af958697b8b3cfe86fb9dca6242d4b7c8
Salil Vanvari <salilvanvari@berkeley.edu>
2020-05-06 02:38:58 -0700

    Add in multiple pieces of paper to enter your words into, this is only frontend change that pretty much breaks the backend for now. We will have to rethink how words are stored in a room.

--- src/AddWordsComponents.js
+++ src/AddWordsComponents.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import "./css/main.css";
 const firebase = require("firebase");
 require("firebase/firestore");
 
 class AddWordsComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+   }
 
   submitValues(values, setSubmitting) {
-    console.log("pre call");
+    console.log(values);
     const db = firebase.firestore();
     const room = "someRoom"; // Replace this with getting dynamic room ID
-
+    var onSubmit = this.props.onSubmit;
     db.collection("rooms").doc(room).set(values)
       .then(function(docRef) {
         console.log(docRef);
         setSubmitting(false);
+        onSubmit();
       })
       .catch(function(error) {
         console.error("Error adding document: ", error);
@@ -23,36 +28,33 @@ class AddWordsComponent extends React.Component {
 
   render() {
     return (
-      <div className="wrapper">
-		  <div className="textInputCard">
-		    <Formik
-		      initialValues={{ 'word1': '', 'word2': '', 'word3': ''}}
-		      // validate to ensure no spaces
-		      onSubmit={(values, { setSubmitting }) => (this.submitValues(values,  setSubmitting))}
-		    >
-		      { ({ isSubmitting }) => (
-		        <Form>
-		          <div>
-			       	  <div> Word 1 </div>
-			          <Field type="text" name="word1" />
-		          </div>
-				  <div>
-			       	  <div> Word 2 </div>
-			          <Field type="text" name="word2" />
-		          </div>
-		          <div>
-			       	  <div> Word 3 </div>
-			          <Field type="text" name="word3" />
-		          </div>
+	  <div className="wordInputCard">
+	    <Formik
+	      initialValues={{ 'word1': '', 'word2': '', 'word3': ''}}
+	      onSubmit={(values, { setSubmitting }) => (this.submitValues(values,  setSubmitting))}
+	    >
+	      { ({ isSubmitting }) => (
+	        <Form>
+	          <div>
+		       	  <div> Word 1 </div>
+		          <Field type="text" name="word1" />
+	          </div>
+			  <div>
+		       	  <div> Word 2 </div>
+		          <Field type="text" name="word2" />
+	          </div>
+	          <div>
+		       	  <div> Word 3 </div>
+		          <Field type="text" name="word3" />
+	          </div>
 
-		          <button type="submit" disabled={isSubmitting}>
-		            Submit
-		          </button>
-		        </Form>
-		      )}
-		    </Formik>
-		  </div> 
-      </div >
+	          <button type="submit" className="blue" disabled={isSubmitting}>
+	            Submit
+	          </button>
+	        </Form>
+	      )}
+	    </Formik>
+	  </div> 
     );
   }
 }
--- src/GameRoom.js
+++ src/GameRoom.js
@@ -3,15 +3,43 @@ import "./css/main.css";
 import {
   withRouter
 } from "react-router-dom";
+import AddWordsComponent from './AddWordsComponents.js'
 
 class GameRoom extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showFirstPaper: true,
+      showSecondPaper: true,
+      showThirdPaper: true,
+    };
+  }
+
+  hideFirstPaper() {
+    this.setState({showFirstPaper : !this.state.showFirstPaper})
+  }
+
+  hideSecondPaper() {
+    this.setState({showSecondPaper : !this.state.showSecondPaper})
+  }
+
+  hideThirdPaper() {
+    this.setState({showThirdPaper : !this.state.showThirdPaper})
+  }
+
   render() {
-    let { id } = this.props.match.params
-    return (
-      <div>
-      	<h3> ID: {id} </h3>
+      let { id } = this.props.match.params
+      let { showFirstPaper, showSecondPaper, showThirdPaper } = this.state;
+      return (
+          <div>
+        <h3> ID: {id} </h3>
+        <div className="wordsWrapper">
+          {showFirstPaper && <AddWordsComponent onSubmit={() => this.hideFirstPaper()}/>}
+          {showSecondPaper && <AddWordsComponent onSubmit={() => this.hideSecondPaper()}/>}
+          {showThirdPaper  && <AddWordsComponent onSubmit={() => this.hideThirdPaper()}/>}
+        </div>
       </div>
-    );
+      );
   }
 }
 
