af3cf2b2cb4f1fc21d5c1c2f41046c7ae7e8e00a
Nik Mathur <nimathur@zendesk.com>
2020-05-08 14:29:15 -0700

    adding empty firestore config

---
+++ app/src/firestoreConfig.js
@@ -0,0 +1,12 @@
+const firestoreConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+export default firestoreConfig;
